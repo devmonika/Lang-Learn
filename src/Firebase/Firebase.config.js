296eb846a781338d90ec7910156d4f9b3c387f20
devmonika <devmonikamomi@gmail.com>
2022-12-22 21:57:25 +0600

    changes

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -12,12 +12,7 @@ const firebaseConfig = {
   messagingSenderId: process.env.REACT_APP_messagingSenderId,
   appId: process.env.REACT_APP_appId,
 
-  // apiKey: "AIzaSyBvTaMUR7gSUVyPs2qY3AE3kVGzfKn4XHg",
-  // authDomain: "lang-learn-29061.firebaseapp.com",
-  // projectId: "lang-learn-29061",
-  // storageBucket: "lang-learn-29061.appspot.com",
-  // messagingSenderId: "588571312323",
-  // appId: "1:588571312323:web:3ef42e283bb00a97fb2276",
+ 
 };
 
 // Initialize Firebase
